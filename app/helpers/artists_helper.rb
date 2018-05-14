b389352bf7a69352f73a88ad5fe8d619036aeec6
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-05-14 03:30:50 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -9,5 +9,6 @@ module ArtistsHelper
     else
     # if song id and artist id are entered
     select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    end
   end
 end
