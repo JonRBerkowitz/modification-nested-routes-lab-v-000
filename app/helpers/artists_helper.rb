ee8934a5caae025fd6efc5d8db63f36607e272f9
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-05-14 03:45:15 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,7 +5,7 @@ module ArtistsHelper
 
   def artist_select(artist_id, artist)
     if artist_id
-      'artist.name'
+      artist.name
     else
     # if song id and artist id are entered
     select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
